e937e0a7f502863d6b2f900b0d4256002489b40a
chintukumar111 <chintudas499@gmail.com>
2025-07-06 10:42:35 +0530

    leap year program in java

---
+++ Basics/Learning.java
@@ -0,0 +1,32 @@
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Learning {
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Year to check leap or not ");
+        int year = sc.nextInt();
+
+        if(year%400==0 || year%4==0 && year%100 != 0) {
+
+
+            System.out.println("It is leap year:: "+ year);
+
+
+        }else {
+            System.out.println("not leap year :"+ year);
+        }
+
+
+    }
+}
