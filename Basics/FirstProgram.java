56dbf6401a6404831e575877588f23144d3e936a
chintukumar111 <chintudas499@gmail.com>
2025-07-06 10:36:13 +0530

    basics question

--- Basics/FirstProgram.java
+++ Basics/FirstProgram.java
@@ -9,36 +9,29 @@ import java.io.*;
 
 public class FirstProgram {
 
-    public static void main (String[] args) throws java.lang.Exception
-    {
-        // your code goes here
-
-        System.out.println("cz star is here ");
 
 
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("enter  the first number");
+        public static void main(String[] args) throws java.lang.Exception {
+            // your code goes here
 
-        int firstNumber = sc.nextInt();
 
-        System.out.println("Enter Second Number ");
-        int secondNumber = sc.nextInt();
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the third number ");
-        int thirdNumber = sc.nextInt();
+            System.out.println("Enter your Number to check to even and odd ");
+            int value = sc.nextInt();
 
-        if(firstNumber>secondNumber && firstNumber>thirdNumber){
-            System.out.println("largest number is : "+ firstNumber);
-        }else if (secondNumber>firstNumber && secondNumber>thirdNumber){
-            System.out.println("Largets number is :"+ secondNumber);
-        }else if(firstNumber == secondNumber && secondNumber== thirdNumber) {
-            System.out.println("all  number is equal :"+ thirdNumber);
-        }
-        else {
-            System.out.println("Largest number is :"+ thirdNumber);
+            if (value % 2 == 0) {
 
-        }
+
+                System.out.println("Number is even " + value);
+
+
+            } else {
+                System.out.println("Number is Odd :" + value);
+            }
+
+
 
     }
 }
