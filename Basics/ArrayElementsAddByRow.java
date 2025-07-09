25cef5aa2c9369cb269c8cdb242df67112360b68
chintukumar111 <chintudas499@gmail.com>
2025-07-09 13:50:20 +0530

    add by row in 2d array

--- Basics/ArrayElementsAddByRow.java
+++ Basics/ArrayElementsAddByRow.java
@@ -1,4 +1,48 @@
 package Basics;
 
 public class ArrayElementsAddByRow {
-}
+
+
+
+
+        public static void main (String args[]) {
+
+
+            int x = 0;
+
+            int [][]a = {{1,2,3},
+                    {4,5,6}};
+
+            System.out.println("Additon by row in 2d array");
+
+            for (int i =0;i<a.length;i++) {
+                int sum = 0;
+
+                for(int j = 0;j<a[i].length;j++) {
+
+
+
+                    sum = sum+a[i][j];
+
+                }
+                System.out.println(i+1+"row sum is : "+sum);
+
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
