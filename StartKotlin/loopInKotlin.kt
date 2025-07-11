5b8f36c93690d22ea2d63ec12e1d36cea44668f6
chintukumar111 <chintudas499@gmail.com>
2025-07-11 22:53:51 +0530

    loop  concept in kotlin

---
+++ StartKotlin/loopInKotlin.kt
@@ -0,0 +1,16 @@
+package StartKotlin
+
+fun main(){
+    println("Loop concept")
+
+    for (i in 1..10){
+
+       var result =  5*i
+        println(result)
+
+    }
+
+    var number = 6;
+    var result1 = number in 1 until 7
+    println(result1)
+}
---
+++ StartKotlin/multiply.kt
@@ -0,0 +1,20 @@
+package StartKotlin
+
+
+
+
+
+    fun main() {
+
+        println("Enter the number you want it to multiply")
+        val input = readLine()?.toInt()
+        var need = 1;
+
+        while(need<=10) {
+
+            println("$input X $need  = "+ need*input!!);
+
+            need++
+        }
+    }
+
