ea60d2d541e55a866e681b2024a144eeefd130d2
chintukumar111 <chintudas499@gmail.com>
2025-07-12 00:21:03 +0530

    array in kotlin

---
+++ StartKotlin/ArrayInKOtlin.kt
@@ -0,0 +1,33 @@
+package StartKotlin
+
+fun main(){
+    println("Array in kotlin learning")
+
+    var arr = arrayOf(1,2,3)
+
+    var a = arrayOf<Int>(4,5,6)
+
+    for((i,e) in arr.withIndex()){
+
+
+
+
+
+        println(i.toString() + e)
+        println()
+
+
+
+    }
+
+    for(value   in 0 until a.size ){
+        println(value)
+    }
+
+    arr.set(0,10)
+    println(arr[0])
+
+  println( arr.get(0))
+
+
+}
