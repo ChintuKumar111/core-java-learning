bd3dbc38497eb370692901f2f5721287f82b55f2
chintukumar111 <chintudas499@gmail.com>
2025-07-13 17:25:13 +0530

    fibonacci series using recursion

--- Basics/FibonaciUsingRecursion.java
+++ Basics/FibonaciUsingRecursion.java
@@ -1,9 +1,31 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class FibonaciUsingRecursion {
 
+public static void  main (String [] args) {
 
-    
+            System.out.println("Fibonacci position Enter");
+            Scanner sc = new Scanner(System.in);
+            int input = sc.nextInt();
+            int result = Fibo(input);
+            System.out.println("Fibonacci position "+input +" = "+ result);
 
 
-}
+
+        }
+
+
+        public static int Fibo(int fiboPosition) {
+
+            int n = fiboPosition;
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return Fibo(n-1)+Fibo(n-2);
+        }
+    }
+
+
