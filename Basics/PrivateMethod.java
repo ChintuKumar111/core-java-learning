810fb47406e7ecf066052487f90b2c777f43b283
chintukumar111 <chintudas499@gmail.com>
2025-07-15 17:36:32 +0530

    private keyword use case

---
+++ Basics/PrivateMethod.java
@@ -0,0 +1,33 @@
+package Basics;
+
+public class PrivateMethod {
+
+        public static void main (String [] args) {
+
+            xrr x = new xrr();
+            x.disp();
+            x.fd();
+
+        }
+
+    }
+
+       class Samsung {
+
+        private  void md() {
+            System.out.println("private method is calling");
+        }
+
+        void fd() {
+            md();
+        }
+
+
+    }
+
+    class xrr extends Samsung {
+
+        void disp() {System.out.println("inherited  class ");}
+
+    }
+
