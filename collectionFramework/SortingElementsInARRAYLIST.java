463cfeb8d2e3b5aaab898d511b996278835866b3
chintukumar111 <chintudas499@gmail.com>
2025-07-17 23:10:10 +0530

    sorting array using arraylist method

---
+++ collectionFramework/SortingElementsInARRAYLIST.java
@@ -0,0 +1,39 @@
+package collectionFramework;
+import java.util.*;
+
+public class SortingElementsInARRAYLIST {
+
+        public static void main (String args[]){
+
+
+            Scanner sc = new Scanner (System.in);
+
+            System.out.println("Enter the number you wants to add INTEGER TYPE");
+
+            int input = sc.nextInt();
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+
+            for (int i=0;i<=input;i++) {
+
+
+
+                list.add(sc.nextInt());
+
+
+            }
+
+            System.out.print("Entered number are  "+list +"\t");
+
+            System.out.println();
+            list.sort(Comparator.reverseOrder());
+
+            System.out.println
+                    ("In decreasing order");
+
+            System.out.println(list);
+
+
+    }
+}
