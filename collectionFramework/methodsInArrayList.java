dcdbcecc77e29ca98437564480bf2f29aa0bd817
chintukumar111 <chintudas499@gmail.com>
2025-07-17 14:35:57 +0530

    array list pre build methods

--- collectionFramework/methodsInArrayList.java
+++ collectionFramework/methodsInArrayList.java
@@ -11,14 +11,38 @@ public class methodsInArrayList {
                 fruits.add("Banana");
                 fruits.add("Mango");
 
-                System.out.println("Fruits: " + fruits);
-                System.out.println("First Fruit: " + fruits.get(0));
-
-                fruits.set(1, "Orange");
                 fruits.remove("Mango");
 
-                System.out.println("Updated List: " + fruits);
-                System.out.println("Size: " + fruits.size());
+                fruits.add("mango");
+
+                fruits.add(0,"Mango");
+
+                fruits.set(3,"Grapes");
+        System.out.println(fruits);
+
+
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
+
+        list.remove(5);
+        System.out.println(list);
+
+        System.out.println(list.contains(2));
+        System.out.println(list);
+        System.out.println(list.size());
+
+        System.out.println(list.isEmpty());
+        System.out.println(list.clone());
+        System.out.println(list.getLast());
+        System.out.println(list.get(3));
             }
         }
 
