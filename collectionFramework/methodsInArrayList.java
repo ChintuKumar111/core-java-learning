b0164c4bbc0777d4217482b284417865de816e00
chintukumar111 <chintudas499@gmail.com>
2025-07-17 14:08:59 +0530

    add by index in java arra list

---
+++ collectionFramework/methodsInArrayList.java
@@ -0,0 +1,18 @@
+package collectionFramework;
+import java.util.*;
+import java.util.ArrayList;
+
+public class methodsInArrayList {
+    public static void  main(String args[]){
+
+        ArrayList<String> a  = new ArrayList<>();
+
+        a.add("Tony Stark");
+
+        a.add(1,"Captain america");
+
+        a.get(1);
+
+        System.out.println(a);
+    }
+}
