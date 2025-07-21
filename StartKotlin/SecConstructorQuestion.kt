3d8f302d18835b422e1286b2deeba864d5a94dd5
chintukumar111 <chintudas499@gmail.com>
2025-07-22 00:39:38 +0530

    using constructor and secondary constructor in kotlin

---
+++ StartKotlin/SecConstructorQuestion.kt
@@ -0,0 +1,26 @@
+package StartKotlin
+
+fun main(){
+    println("Question from you")
+
+    val book1 = Books("Kotlin basics",299.0)
+
+
+    val book2 = Books("Advanced Kotlin")
+
+
+    book1.printDetails()
+    book2.printDetails()
+}
+
+class Books(val title :String ,val price:Double){
+
+    constructor(title: String):this(title,399.0)
+
+    init{
+        println("Book Initialize : $title - RS $price")
+    }
+
+    fun printDetails(){
+        println("Title : $title ,Price $price")
+    }}
